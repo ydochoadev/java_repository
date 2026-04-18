e81229ae235e9134c6c45335f0f6ecf4c0135998
ydochoadev <ydochoadev@gmail.com>
2026-04-18 18:58:49 -0500

    Se  creó clase Product usando Lombock

---
+++ microservices-ecommerce/product-service/src/main/java/com/ecommerce/product_service/model/Product.java
@@ -0,0 +1,28 @@
+package com.ecommerce.product_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+// @Document le dice a spring-data que cuando se guarde esta clase, lo colocará dentro de una colección
+@Document(value = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+    
+    @Id
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
