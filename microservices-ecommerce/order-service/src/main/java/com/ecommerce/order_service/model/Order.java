bdbf10ecac891abb347e77a0d1dc6cb431b0c83c
ydochoadev <ydochoadev@gmail.com>
2026-05-10 10:52:13 -0500

    feat: Se creóaron los modelos OrderLineItems y Order

---
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/model/Order.java
@@ -0,0 +1,36 @@
+package com.ecommerce.order_service.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true) // orphanRemoval es clave para Updates
+    @JoinColumn(name = "order_id") // Crea la FK en la tabla de items
+    private List<OrderLineItems> orderLineItemsList;
+}
---
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/model/OrderLineItems.java
@@ -0,0 +1,30 @@
+package com.ecommerce.order_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private BigDecimal price;
+    private Integer quantity;
+}
