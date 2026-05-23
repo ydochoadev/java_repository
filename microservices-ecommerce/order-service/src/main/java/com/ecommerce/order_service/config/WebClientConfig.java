ad574121a0584777029907ab36efc3e996b2295f
ydochoadev <ydochoadev@gmail.com>
2026-05-23 17:29:48 -0500

    feat: Se creó interface para comunicación con inventory-service

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
@@ -1,14 +1,29 @@
 package com.ecommerce.order_service.config;
 
+import com.ecommerce.order_service.service.client.InventoryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient webClientBuilder() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8082")
+                .build();
+    }
+
+    @Bean
+    public InventoryClient inventoryClient(WebClient webClient) {
+        // Para ejecutar la petición usando webClient
+        WebClientAdapter webClientAdapter = WebClientAdapter.create(webClient);
+        // Implementa la interface InventoryClient
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(webClientAdapter).build();
+
+        return factory.createClient(InventoryClient.class);
     }
 }
