2164669b76b40dfa5a0092641478cd8041f9bdb1
ydochoadev <ydochoadev@gmail.com>
2026-05-30 21:13:22 -0500

    feat: Se agregó anotación RefreshScope

--- microservices-ecommerce/product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ microservices-ecommerce/product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.ecommerce.product_service.controller;
 import com.ecommerce.product_service.dto.ProductRequestDTO;
 import com.ecommerce.product_service.dto.ProductResponseDTO;
 import com.ecommerce.product_service.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
+@RefreshScope
 public class ProductController {
 
     private final ProductService productService;
+
+    @Value("${app.maintenance.message: Sistema Operativo}")
+    private String maintenanceMessage;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,7 +33,8 @@ public class ProductController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponseDTO> findAllProducts() {
+    public List<ProductResponseDTO> findAllProducts(HttpServletResponse response) {
+        response.addHeader("X-Maintenance-Message", maintenanceMessage);
         return productService.getAllProducts();
     }
 
