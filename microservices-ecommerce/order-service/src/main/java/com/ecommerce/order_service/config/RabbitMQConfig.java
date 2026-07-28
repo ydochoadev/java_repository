13ed513418114eb0aa78b93dc8724801995b74c2
ydochoadev <ydochoadev@gmail.com>
2026-07-28 16:04:29 -0500

    feat: Se agrega Bean para configurar el exchange, donde se enviarán los mensajes para que sean enrutados

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
@@ -1,14 +1,26 @@
 package com.ecommerce.order_service.config;
 
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 
 public class RabbitMQConfig {
-    /* Bean para convertir la serialización java a Jackson
-   para convertir los mensajes a json */
+
+    public static final String EXCHANGE_NAME = "order-events";
+
+    // Bean para convertir la serialización java a Jackson para convertir los mensajes a json
     @Bean
     public MessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    // 1. Declara infraestructura: al correr app, bean contacta a RabbitMQ y asegura que exchange exista
+    // 2. Desacoplar: order-service envía msj hasta el exchange (no a las colas)
+    // 3. Flexibilidad: Enrutamiento por patrones. Permite que msj tengan temas asociados (routing key)
+    // y loc consumidores se suscriben a los temas que les interesa
+    @Bean
+    public TopicExchange orderEventsExchange() {
+        return new TopicExchange(EXCHANGE_NAME);
     }
 }
