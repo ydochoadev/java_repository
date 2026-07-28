811b7813b3e012769d2bd79f77013ad5ac230d5a
ydochoadev <ydochoadev@gmail.com>
2026-07-28 16:03:29 -0500

    feat: Se agrega email al DTO OrderRequest, necesario para el mensaje enviado a RabbitMQ

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
@@ -1,6 +1,8 @@
 package com.ecommerce.order_service.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +14,12 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequest {
+
     @NotEmpty(message = "La orden debe contener al menos un item")
     @Valid
     private List<OrderLineItemsRequest> orderLineItemsDtoList;
+
+    @NotBlank(message = "El correo es requerido")
+    @Email(message = "El formato de correo no es válido")
+    private String email;
 }
