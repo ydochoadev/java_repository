102bae7dff452cfe74413f69b7905a61abc40cb0
ydochoadev <ydochoadev@gmail.com>
2026-07-28 10:45:45 -0500

    feat: Se agregó configuración para usar el convertidor de los mensajes rabbitMQ

---
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.ecommerce.order_service.config;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMQConfig {
+    /* Bean para convertir la serialización java a Jackson
+   para convertir los mensajes a json */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+}
