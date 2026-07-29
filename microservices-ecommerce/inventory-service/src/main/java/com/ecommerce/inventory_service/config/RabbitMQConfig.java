4168bbddbeb21fedc5b14aa398d2dabec384d7d4
ydochoadev <ydochoadev@gmail.com>
2026-07-29 15:25:28 -0500

    feat: Configuración de la cola y binding que une el exchange con la cola

--- microservices-ecommerce/inventory-service/src/main/java/com/ecommerce/inventory_service/config/RabbitMQConfig.java
+++ microservices-ecommerce/inventory-service/src/main/java/com/ecommerce/inventory_service/config/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package com.ecommerce.inventory_service.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +12,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    /* Bean para convertir la serialización java a Jackson
-    para convertir los mensajes a json */
+    // Bean para convertir la serialización java a Jackson para convertir los mensajes a json
     @Bean
     public MessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    // Cola: Donde se almacenan los mensajes esperando ser leídos
+    @Bean
+    public Queue inventoryQueue() {
+        // duration: SI RabbitMQ se reinicia, la cola sobrevive
+        return new Queue("inventory-queue", true);
+    }
+
+    // Distribuidor: Recibe el mensaje y decide a dónde lo almacena
+    @Bean
+    public TopicExchange orderEventsExchange() {
+        return new TopicExchange("order-events");
+    }
+
+    // Bean que une: Conecta el exchange con la cola:
+    // Si llega un msj con la etiqueta 'order.placed', envía una copia a la cola inventory-queue
+    @Bean
+    public Binding binding(Queue inventoryQueue, TopicExchange orderEventsExchange) {
+        return BindingBuilder.bind(inventoryQueue).to(orderEventsExchange).with("order.placed");
     }
 }
