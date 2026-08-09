9f9607283dc018c6eb52090f267e6fb5861ab2db
ydochoadev <ydochoadev@gmail.com>
2026-08-09 12:13:25 -0500

    feat: Se agrega código para enviar correo

--- microservices-ecommerce/notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
+++ microservices-ecommerce/notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
@@ -1,26 +1,39 @@
 package com.ecommerce.notification_service.listener;
 
 import com.ecommerce.notification_service.event.OrderPlaceEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class OrderEventsListener {
+
+    private final JavaMailSender mailSender;
 
     // @RabbitListener: Hace que springboot arranque un proceso en 2° plano (hilo) con una conexión a RabbiTMQ
     @RabbitListener(queues = "notification-queue")
     public void handleOrderPlaceEvent(OrderPlaceEvent event) {
         log.info("Evento recibido en Inventario para la orden: {}", event.orderNumber());
-        event.items().forEach(item -> {
-            try {
-                log.info("Enviando correo de confirmacióna : {}", event.email());
+        try {
+            log.info("Enviando correo de confirmación a : {}", event.email());
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("pedidos@ecommerce.com");
+            message.setTo(event.email());
+            message.setSubject("Orden Confirmada - " + event.orderNumber());
+            message.setText(" Hola! \n\n" +
+                    "Tu pedido con número " + event.orderNumber() + " ha sidorecibido exitosamente. \n" +
+                    "Gracias por comprar con nosortros");
+            mailSender.send(message);
 
-                log.info("Correo enviando exitosamente para la orden {}", event.orderNumber());
-            } catch (Exception e) {
-                log.error("Error al enviar correo: {}", e.getMessage());
-            }
-        });
+            log.info("Correo enviando exitosamente para la orden {}", event.orderNumber());
+        } catch (Exception e) {
+            log.error("Error al enviar correo: {}", e.getMessage());
+        }
+
     }
 }
