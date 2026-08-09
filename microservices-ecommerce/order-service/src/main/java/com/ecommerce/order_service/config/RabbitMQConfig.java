d0800b03ca8ffbfc0a912bf765a364c52d72c998
ydochoadev <ydochoadev@gmail.com>
2026-08-09 17:08:46 -0500

    fix: Se agrega notación @Configuration para solventar error

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
@@ -4,7 +4,9 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
 
     public static final String EXCHANGE_NAME = "order-events";
