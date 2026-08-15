c39f7dfa26c87d29f5d827eae8bb39e5cd361b20
ydochoadev <ydochoadev@gmail.com>
2026-08-15 16:41:39 -0500

    fix: Se modifica el listener de inventory para confirmar la orden (si hay stock) o cancelarla (si no hay stock)

--- microservices-ecommerce/inventory-service/src/main/java/com/ecommerce/inventory_service/listener/OrderEventsListener.java
+++ microservices-ecommerce/inventory-service/src/main/java/com/ecommerce/inventory_service/listener/OrderEventsListener.java
@@ -1,10 +1,12 @@
 package com.ecommerce.inventory_service.listener;
 
+import com.ecommerce.inventory_service.event.OrderCancelledEvent;
 import com.ecommerce.inventory_service.event.OrderPlaceEvent;
 import com.ecommerce.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -13,18 +15,38 @@ import org.springframework.stereotype.Component;
 public class OrderEventsListener {
 
     private final InventoryService inventoryService;
+    private final RabbitTemplate rabbitTemplate;
 
     // @RabbitListener: Hace que springboot arranque un proceso en 2° plano (hilo) con una conexión a RabbiTMQ
     @RabbitListener(queues = "inventory-queue")
     public void handleOrderPlaceEvent(OrderPlaceEvent event) {
         log.info("Evento recibido en Inventario para la orden: {}", event.orderNumber());
-        event.items().forEach(item -> {
-            try {
-                inventoryService.reduceStock(item.sku(), item.quantity());
-                log.info("Stock descontado para SKU {} - Cantidad {}", item.sku(), item.quantity());
-            } catch (Exception e) {
-                log.error("Error al procesar el stock para SKU {}: {}", item.sku(), e.getMessage());
+        try {
+            boolean allProductsInStock = event.items().stream()
+                    .allMatch(item -> inventoryService.isInStock(item.sku(), item.quantity()));
+            if (!allProductsInStock) {
+                cancelOrder(event, "Stock insuficiente en uno o más productos");
+                return;
             }
-        });
+            // Si hay stock, reducirlo
+            event.items().forEach(item -> {
+                inventoryService.reduceStock(item.sku(), item.quantity());
+            });
+            // Confirmar
+            rabbitTemplate.convertAndSend("order-events", "order.confirmed", event);
+
+            log.info("Stock descontado para SKU {}", event.orderNumber());
+        } catch (Exception e) {
+            log.error("Error al procesar el stock: {}", e.getMessage());
+            cancelOrder(event, "Error técnico en el procesamiento de inventario");
+        }
+    }
+
+    private void cancelOrder(OrderPlaceEvent event, String reason) {
+        OrderCancelledEvent orderCancelledEvent = new OrderCancelledEvent(event.orderNumber(),
+                event.orderNumber(),
+                reason);
+        // Cuando no hay stock suficiente, se dispara otro evento
+        rabbitTemplate.convertAndSend("order-events", "order.cancelled", orderCancelledEvent);
     }
 }
