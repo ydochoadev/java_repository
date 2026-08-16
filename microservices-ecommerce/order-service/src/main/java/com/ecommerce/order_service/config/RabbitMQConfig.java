ecf25355d8848dddc69e863868a5184a480bce52
ydochoadev <ydochoadev@gmail.com>
2026-08-16 10:53:46 -0500

    fix: Se modifica config de Rabbit para agregar cola para escuchar las órdenes canceladas o confirmadas por inventory-service

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.ecommerce.order_service.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -24,5 +27,31 @@ public class RabbitMQConfig {
     @Bean
     public TopicExchange orderEventsExchange() {
         return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    // Cola para escuchar a inventory-service de las órdenes confirmadas
+    @Bean
+    public Queue orderConfirmedQueue() {
+        return new Queue("order-confirmed-queue", true);
+    }
+
+    // Bean que une: Conecta el exchange con la cola:
+    // Si llega un msj con la etiqueta 'order.confirmed', envía una copia a la cola order-confirmed-queue
+    @Bean
+    public Binding confirmedBinding(Queue orderConfirmedQueue, TopicExchange orderEventsExchange) {
+        return BindingBuilder.bind(orderConfirmedQueue).to(orderEventsExchange).with("order.confirmed");
+    }
+
+    // Cola para escuchar a inventory-service de las órdenes canceladas
+    @Bean
+    public Queue orderCancelledQueue() {
+        return new Queue("order-cancelled-queue", true);
+    }
+
+    // Bean que une: Conecta el exchange con la cola:
+    // Si llega un msj con la etiqueta 'order.confirmed', envía una copia a la cola order-cancelled-queue
+    @Bean
+    public Binding cancelledBinding(Queue orderCancelledQueue, TopicExchange orderEventsExchange) {
+        return BindingBuilder.bind(orderCancelledQueue).to(orderEventsExchange).with("order.cancelled");
     }
 }
