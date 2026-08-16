316919051c66cc6abee6ab52a1c1fece0a4f9c04
ydochoadev <ydochoadev@gmail.com>
2026-08-16 11:05:21 -0500

    fix: Se magrega nueva función en el repositorio para buscar por orderNumber

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/repository/OrderRepository.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/repository/OrderRepository.java
@@ -4,8 +4,11 @@ import com.ecommerce.order_service.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     // Búsqueda por userId
     List<Order> findByUserId(String userId);
+
+    Optional<Order> findByOrderNumber(String orderNumber);
 }
