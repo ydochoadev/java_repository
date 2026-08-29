76b7fa4e123988b2eb72a6965db260a25d054646
ydochoadev <ydochoadev@gmail.com>
2026-08-29 11:22:13 -0500

    fix: Se modifica el listener de order para escuchar las órdenes confirmadas o declinadas

--- microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/listener/OrderEventListener.java
+++ microservices-ecommerce/order-service/src/main/java/com/ecommerce/order_service/listener/OrderEventListener.java
@@ -1,6 +1,7 @@
 package com.ecommerce.order_service.listener;
 
-import com.ecommerce.order_service.event.OrderPlaceEvent;
+import com.ecommerce.order_service.event.OrderCancelledEvent;
+import com.ecommerce.order_service.event.OrderConfirmedEvent;
 import com.ecommerce.order_service.model.OrderStatus;
 import com.ecommerce.order_service.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +17,20 @@ public class OrderEventListener {
     private final OrderService orderService;
 
     @RabbitListener(queues = "order-confirmed-queue")
-    public void handleOrderConfirmed(OrderPlaceEvent event) {
+    public void handleOrderConfirmed(OrderConfirmedEvent event) {
+        if (event.orderNumber() == null) {
+            log.error("OrderConfirmedEvent con orderNumber en null. Descartando");
+            return;
+        }
         orderService.updateOrderStatus(event.orderNumber(), OrderStatus.CONFIRMED);
     }
 
     @RabbitListener(queues = "order-cancelled-queue")
-    public void handleOrderCancelled(OrderPlaceEvent event) {
+    public void handleOrderCancelled(OrderCancelledEvent event) {
+        if (event.orderNumber() == null) {
+            log.error("OrderCancelledEvent con orderNumber en null. Descartando");
+            return;
+        }
         orderService.updateOrderStatus(event.orderNumber(), OrderStatus.CANCELLED);
     }
 }
