8b38cfb588e9221d636576c848f8a8667c0a9aef
ydochoadev <ydochoadev@gmail.com>
2026-09-12 10:38:58 -0500

    feat: Se agrega dependencia mapstruct y se modifica clase Transaction para colocar getter y setter en vez de data

--- atlas/src/main/java/com/atlas/bank/atlas/transaction/model/Transaction.java
+++ atlas/src/main/java/com/atlas/bank/atlas/transaction/model/Transaction.java
@@ -6,14 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaction {
