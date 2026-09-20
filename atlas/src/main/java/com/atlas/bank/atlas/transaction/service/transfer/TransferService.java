6d5a2cabbe32beed0f697244d24e0be219589070
ydochoadev <ydochoadev@gmail.com>
2026-09-20 15:18:46 -0500

    feat: SSe modifica TransferService para que actúe como publicador dele evento TransactionExecutedEvent

--- atlas/src/main/java/com/atlas/bank/atlas/transaction/service/transfer/TransferService.java
+++ atlas/src/main/java/com/atlas/bank/atlas/transaction/service/transfer/TransferService.java
@@ -7,8 +7,10 @@ import com.atlas.bank.atlas.transaction.exeption.InsufficientFundsException;
 import com.atlas.bank.atlas.transaction.model.Transaction;
 import com.atlas.bank.atlas.account.repoditory.AccountRepository;
 import com.atlas.bank.atlas.transaction.repository.TransactionRepository;
+import com.atlas.bank.atlas.transaction.service.event.TransactionExecutedEvent;
 import com.atlas.bank.atlas.transaction.service.factory.TransactionFactory;
 import com.atlas.bank.atlas.transaction.service.fee.FeeCalculator;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,13 +22,16 @@ public class TransferService extends TransactionProcessor<TransferContext> imple
 
     private final AccountRepository accountRepository;
     private final List<FeeCalculator> feeCalculators; // Se tiene TODA las implementaciones
+    private final ApplicationEventPublisher eventPublisher;
 
     public TransferService(TransactionRepository transactionRepository,
                            AccountRepository accountRepository,
-                           List<FeeCalculator> feeCalculators) {
+                           List<FeeCalculator> feeCalculators,
+                           ApplicationEventPublisher eventPublisher) {
         super(transactionRepository);
         this.accountRepository = accountRepository;
         this.feeCalculators = feeCalculators;
+        this.eventPublisher = eventPublisher;
     }
 
     @Override
@@ -39,7 +44,17 @@ public class TransferService extends TransactionProcessor<TransferContext> imple
                 .orElseThrow(() -> new AccountNotFoundException(toId));
 
         // process => aplica el patrón Template Method
-        return process(new TransferContext(from, to, amount));
+        Transaction transaction = process(new TransferContext(from, to, amount));
+        // Lanzar el evento
+        eventPublisher.publishEvent(new TransactionExecutedEvent(
+                transaction.getId(),
+                transaction.getType(),
+                transaction.getSourceAccountId(),
+                transaction.getTargetAccountId(),
+                transaction.getAmount(),
+                transaction.getFee()
+        ));
+        return transaction;
     }
 
     @Override
