320207ecd2ed06c0793fd1d96700a6910f01c4d6
ydochoadev <ydochoadev@gmail.com>
2026-09-25 20:24:47 -0500

    feat:Se agregan enums para manejar los tipo y estados de transferencias

---
+++ atlas/src/main/java/com/atlas/bank/atlas/transaction/model/TransactionStatus.java
@@ -0,0 +1,5 @@
+package com.atlas.bank.atlas.transaction.model;
+
+public enum TransactionStatus {
+    EXECUTED
+}
---
+++ atlas/src/main/java/com/atlas/bank/atlas/transaction/model/TransactionType.java
@@ -0,0 +1,7 @@
+package com.atlas.bank.atlas.transaction.model;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER,
+}
